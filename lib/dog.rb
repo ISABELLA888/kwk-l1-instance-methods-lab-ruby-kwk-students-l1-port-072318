d7738090fa3a13e5aa23af2ea34646993693a2e4
ISABELLA888 <ipcastro888@gmail.com>
2018-07-26 22:17:35 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,41 @@
-# dog.rb
+# # dog.rb
+# class Dog
+#   def initialize(bark, sit)
+#     @bark = bark
+#     @sit = sit
+#   end
+  
+#   def bark
+#     @bark
+#   end
+  
+#   def sit
+#     @sit
+#   end
+  
+# end
+
+# dog = Dog.new("Woof!", "The Dog is sitting")  
+
+# puts dog.bark
+
+
+class Person
+  def initialize(talk, walk)
+    @talk = talk
+    @walk = walk
+  end
+  
+  def talk
+    @talk
+  end
+  
+  def walk
+    @walk
+  end
+end
+
+person = Person.new("Hello World!", "The Person is walking")
+
+puts person.walk
+puts person.talk
